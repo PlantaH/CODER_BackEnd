de2e3c8c4df812b5da23e5db65485c273e1bdf93
PlantaH <laplantah@gmail.com>
2021-12-16 21:27:06 -0300

    Clase 2

---
+++ clases.js
@@ -0,0 +1,55 @@
+class Libro{
+    constructor(nombre, autor) {
+        this.nombre = nombre;
+        this.autor = autor;         
+    } 
+}
+
+class Usuario {
+    constructor(nombre, apellido,libros,mascotas) {
+
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+        
+    }
+
+    getFullName(){
+        return (this.nombre + ' ' + this.apellido)
+    }
+
+    addMascota(mascota){
+        this.mascotas = [...this.mascotas,mascota]
+    }
+
+    countMascotas(){
+        return (this.mascotas.length)
+    }
+
+    addBook(nombre,autor){
+        this.libros = [...this.libros,new Libro(nombre,autor)]
+    }
+
+    getBookNames(){
+        return (this.libros.map((libro) => libro.nombre ))
+    }   
+    
+}
+
+let book =  Array(new Libro('El señor de los anillos','Tolkien'))
+
+let user = new Usuario('hernan','dupuy',book,['perro','gato','tortuga'])
+
+console.log(user.getFullName())
+
+console.log(user.countMascotas())
+
+user.addMascota('canario')
+
+console.log(user.countMascotas())
+
+user.addBook('El tunel','Sabato')
+user.addBook('Rayuela','Cortazar')
+
+console.log(user.getBookNames())
